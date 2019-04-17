42f31ac313ad4381866dcea5247b9d4748e82cb9
williankal <kenzo.willian@hotmail.com>
2019-04-17 13:43:27 -0300

    MultiInsper

--- ep1.py
+++ ep1.py
@@ -5,64 +5,72 @@
 # - aluno B: Willian Asanuma, williankal@al.insper.edu.br
 # - aluno C: Gabriel Parfan,  gabrielpg1@al.insper.edu.br
 def carregar_cenarios():
-    cenarios = {
-        "saguao": {
-            "titulo": "Saguao do perigo",
-            "descricao": "Voce esta no saguao de entrada do insper",
-            "opcoes": {
-                "catraca": "Passar pela catraca",
-                "biblioteca": "Ir para a biblioteca",
-                "desistir": "Desistir e ir embora"
+        cenarios = {
+            "saguao": {
+                "titulo": "Saguao do perigo",
+                "descricao": "Voce esta no saguao de entrada do insper",
+                "opcoes": {
+                    "catraca": "Passar pela catraca",
+                    "biblioteca": "Ir para a biblioteca",
+                    "desistir": "Desistir e ir embora"
+                }
+            },
+            "catraca": {
+                "titulo": "O portao giratorio",
+                "descricao": "Voce esta nas catracas do Saguao do perigo",
+                "opcoes": {
+                    "saguao": "Voltar ao Saguao do perigo",
+                    "elevador": "Ir para o transportador magico",
+                    "escada": "Ir para a escadaria do infinito"
+                }
+            },
+            "escada": {
+                "titulo": "Escadaria do infinito",
+                "descricao": "Voce subiu a Escadaria do infinito e agora está no 2o andar",
+                "opcoes": {
+                    "catraca": "Voltar para o portao giratorio",
+                    "corredor andar 2": "Ir para o corredor do segundo andar"
+                }
+            },
+            "biblioteca": {
+                "titulo": "Santuario da sabedoria",
+                "descricao": "Voce esta na biblioteca do insper",
+                "opcoes": {
+                    "saguao": "Voltar para o Saguao do perigo"
+                }
+            },
+            "corredor andar 2": {
+                "titulo": "Corredor do segundo andar",
+                "descricao": "Voce esta caminhando pelo corredor do segundo andar",
+                "opcoes": {
+                    "catraca": "Descer as escadas para o portao giratorio"
+                }
+            },
+            "elevador": {
+                "titulo": "Transportador magico",
+                "descricao": "Voce esta na biblioteca do insper",
+                "opcoes": {
+                    "saguao": "Voltar para o Saguao do perigo"
+                }
+            },
+            "explorar biblioteca": {
+                "titulo": "Escadaria do infinito",
+                "descricao": "Voce subiu a Escadaria do infinito e agora está no 2o andar",
+                "opcoes": {
+                    "catraca": "Voltar para o portao giratorio",
+                    "corredor andar 2": "Ir para o corredor do segundo andar"
+                }
+            },
+            "MultiInsper": {
+                "titulo": "Apoio",
+                "descricao": "Voce subiu a Escadaria do infinito e agora está no 2o andar",
+                "opcoes": {
+                    "catraca": "Voltar para o portao giratorio",
+                    "corredor andar 2": "Ir para o corredor do segundo andar"
+                }
             }
-        },
-        "catraca": {
-            "titulo": "O portao giratorio",
-            "descricao": "Voce esta nas catracas do Saguao do perigo",
-            "opcoes": {
-                "saguao": "Voltar ao Saguao do perigo",
-                "elevador": "Ir para o transportador magico",
-                "escada": "Ir para a escadaria do infinito"
-            }
-        },
-        "escada": {
-            "titulo": "Escadaria do infinito",
-            "descricao": "Voce subiu a Escadaria do infinito e agora está no 2o andar",
-            "opcoes": {
-                "catraca": "Voltar para o portao giratorio",
-                "corredor andar 2": "Ir para o corredor do segundo andar"
-            }
-        },
-        "biblioteca": {
-            "titulo": "Santuario da sabedoria",
-            "descricao": "Voce esta na biblioteca do insper",
-            "opcoes": {
-                "saguao": "Voltar para o Saguao do perigo"
-            }
-        },
-        "corredor andar 2": {
-            "titulo": "Corredor do segundo andar",
-            "descricao": "Voce esta caminhando pelo corredor do segundo andar",
-            "opcoes": {
-                "catraca": "Descer as escadas para o portao giratorio"
-            }
-        },
-        "elevador": {
-            "titulo": "Transportador magico",
-            "descricao": "Voce esta na biblioteca do insper",
-            "opcoes": {
-                "saguao": "Voltar para o Saguao do perigo"
-            }
-        },
-        "explorar biblioteca": {
-            "titulo": "Escadaria do infinito",
-            "descricao": "Voce subiu a Escadaria do infinito e agora está no 2o andar",
-            "opcoes": {
-                "catraca": "Voltar para o portao giratorio",
-                "corredor andar 2": "Ir para o corredor do segundo andar"
-            }
+            
         }
-        
-    }
     nome_cenario_atual = "saguao"
     return cenarios, nome_cenario_atual
 
@@ -176,6 +184,16 @@ def main():
         print("Você morreu!")
 
 print ("banana frita")
+
+#items
+class ChaveMestra(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Chave Mestra",
+                         description="Uma chave mestra que dá acesso a todas as salas da insper.".format(str(self.amt)),
+                         value=self.amt)
+    
+    
 # Programa principal.
 if __name__ == "__main__":
     main()
