ff62af56e46337bb6aa7407b7d306b0426f4df7d
GParfan <49314033+GParfan@users.noreply.github.com>
2019-04-22 09:16:34 -0300

    arquivo_itens.py adicionado

---
+++ arquivo_itens.py
@@ -0,0 +1,18 @@
+{
+    "carteirinha": {
+        "titulo": "carteirinha",
+        "dano": 0
+        },
+    "atestado": {
+        "titulo": "atestado",
+        "dano": 5
+        },
+    "punhos": {
+        "titulo": "punhos",
+        "dano": 3
+        },
+    "chave": {
+        "titulo": "chave",
+        "dano": 0
+        }
+    }
